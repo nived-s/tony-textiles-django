fad08bcca06d05f8d355d207ae732e15fb1530f5
nived-s <nivedsreenilayam@gmail.com>
2023-03-13 23:27:34 +0530

    navbar added but login button linking not done

--- tony_textiles/store/urls.py
+++ tony_textiles/store/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.store, name="store"),        #base url
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
-}
+]
